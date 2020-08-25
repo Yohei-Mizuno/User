bf6c6af8d331ca393e8f1c8de430dc317366c265
Yohei-Mizuno <mizyotama2525@gmail.com>
2020-08-25 20:57:37 +0900

    First commit

---
+++ User.py
@@ -0,0 +1,19 @@
+class User:
+    def __init__(self,name):
+        self.name = name
+
+
+if __name__ == '__main__':
+    user1 = User('Bob')
+    print(user1.name)
+
+    user2 = User('Joey')
+    print(user2.name)
+
+    user3 = User('Chandler')
+    print(user3.name)
+
+
+
+
+
